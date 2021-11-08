b7b4a275131246c84a0235373bcef41822607e45
Yury Bialou <Pradd.by@gmail.com>
2021-11-08 10:33:24 +0300

    feat: add index for stream

---
+++ streams/index.js
@@ -0,0 +1,13 @@
+const MyReadStream = require("./MyReadStream");
+const MyWriteStream = require("./MyWriteStream");
+const CaesarTransformStream = require("./CaesarTransformStream");
+const AtbashTransformStream = require("./AtbashTransformStream");
+const ROTTransformStream = require("./ROTTransformStream");
+
+module.exports = {
+  MyReadStream,
+  MyWriteStream,
+  CaesarTransformStream,
+  AtbashTransformStream,
+  ROTTransformStream,
+};
