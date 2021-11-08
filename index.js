b1844b15e12ee0e4e1f37f8dc332347487ba68e9
Yury Bialou <Pradd.by@gmail.com>
2021-11-08 10:37:51 +0300

    feat: add main file - index js

---
+++ index.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const { pipeline } = require("stream");
+const {
+  entryArgv,
+  isDuplicateOption,
+  nameOption,
+  isDir,
+  parserConfig,
+} = require("./helpers");
+const { MyReadStream, MyWriteStream } = require("./streams");
+const {
+  NotFoundError,
+  DuplicateError,
+  BadPathError,
+  BadValidationError,
+} = require("./errors");
+const { validationConfig } = require("./validation");
+
+const argv = process.argv.slice(2);
+
+const configs = entryArgv(argv, "-c", "--config");
+const inputs = entryArgv(argv, "-i", "--input");
+const outputs = entryArgv(argv, "-o", "--output");
+
+const config = configs && nameOption(argv, configs);
+const fileForRead = inputs && path.join(__dirname, nameOption(argv, inputs));
+const fileForWrite = outputs && path.join(__dirname, nameOption(argv, outputs));
+
+try {
+  if (!configs) {
+    throw new NotFoundError("Missing '-c' or '--config' option!")
+  } else if (configs && isDuplicateOption(configs)) {
+    throw new DuplicateError("Duplicated config option!");
+  } else if (inputs && isDuplicateOption(inputs)) {
+    throw new DuplicateError("Duplicated input option!");
+  } else if (outputs && isDuplicateOption(outputs)) {
+    throw new DuplicateError("Duplicated output option!");
+  } else if (fileForRead && isDir(fileForRead)) {
+    throw new BadPathError(`${fileForRead} is directory, but need file!`);
+  } else if (fileForWrite && isDir(fileForWrite)) {
+    throw new BadPathError(`${fileForWrite} is directory, but need file!`);
+  } else if (!validationConfig(config)) {
+    throw new BadValidationError(`This config "${config}" is invalid!`);
+  }
+} catch (err) {
+  process.stderr.write(`${err}`);
+  process.exit(1);
+}
+
+const arrTransform = parserConfig(config);
+const read = inputs ? new MyReadStream(fileForRead) : process.stdin;
+const write = outputs ? new MyWriteStream(fileForWrite) : process.stdout;
+
+pipeline(
+  read,
+  ...arrTransform,
+  write,
+  (err) => {
+    if (err) {
+      process.stderr.write(`Unknow error: ${err.message}`);
+      process.exit(1);
+    }
+  }
+);
+
+process.on('SIGINT', () => process.exit(0));
