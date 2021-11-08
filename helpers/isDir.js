14f4ec841bfbcb280c611b988e87fa7664d66ca2
Yury Bialou <Pradd.by@gmail.com>
2021-11-08 10:35:04 +0300

    feat: create fun isDir

---
+++ helpers/isDir.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const isDir = (pathFile) => {
+  try {
+    const stats = fs.statSync(pathFile);
+    if (stats.isDirectory()) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    process.stderr.write(`Not fount file: This file "${path.basename(pathFile)}"" do not exist!`);
+    process.exit(1);
+  }
+}
+
+module.exports = isDir;
