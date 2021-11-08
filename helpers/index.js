18a90d95926f93684479c4e5488f186c20715937
Yury Bialou <Pradd.by@gmail.com>
2021-11-08 10:36:01 +0300

    feat: add index for helpers

---
+++ helpers/index.js
@@ -0,0 +1,13 @@
+const isDuplicateOption = require("./isDuplicateOption");
+const entryArgv = require("./entryArgv");
+const nameOption = require("./nameOption");
+const isDir = require("./isDir");
+const parserConfig = require("./parserConfig");
+
+module.exports = {
+  isDuplicateOption,
+  entryArgv,
+  nameOption,
+  isDir,
+  parserConfig,
+} 
