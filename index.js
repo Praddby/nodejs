fcdcf4d411e952eb061575d84bed1b9185049348
Yury Bialou <Pradd.by@gmail.com>
2021-11-09 20:50:56 +0300

    refactor: refactor index js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const { pipeline } = require("stream");
 const {
   entryArgv,
@@ -24,22 +23,28 @@ const inputs = entryArgv(argv, "-i", "--input");
 const outputs = entryArgv(argv, "-o", "--output");
 
 const config = configs && nameOption(argv, configs);
-const fileForRead = inputs && path.join(__dirname, nameOption(argv, inputs));
-const fileForWrite = outputs && path.join(__dirname, nameOption(argv, outputs));
+const nameInput = inputs && nameOption(argv, inputs);
+const nameOutput = outputs && nameOption(argv, outputs);
 
 try {
   if (!configs) {
     throw new NotFoundError("Missing '-c' or '--config' option!")
-  } else if (configs && isDuplicateOption(configs)) {
+  } else if (!config) {
+    throw new NotFoundError("Missing value for '-c' or '--config' option!");
+  } else if (!nameInput) {
+    throw new NotFoundError("Missing value for '-i' or '--input' option!");
+  } else if (!nameOutput) {
+    throw new NotFoundError("Missing value for '-o' or '--output' option!");
+  } else if (isDuplicateOption(configs)) {
     throw new DuplicateError("Duplicated config option!");
   } else if (inputs && isDuplicateOption(inputs)) {
     throw new DuplicateError("Duplicated input option!");
   } else if (outputs && isDuplicateOption(outputs)) {
     throw new DuplicateError("Duplicated output option!");
-  } else if (fileForRead && isDir(fileForRead)) {
-    throw new BadPathError(`${fileForRead} is directory, but need file!`);
-  } else if (fileForWrite && isDir(fileForWrite)) {
-    throw new BadPathError(`${fileForWrite} is directory, but need file!`);
+  } else if (isDir(nameInput)) {
+    throw new BadPathError(`${nameInput} is directory, but need file!`);
+  } else if (isDir(nameOutput)) {
+    throw new BadPathError(`${nameOutput} is directory, but need file!`);
   } else if (!validationConfig(config)) {
     throw new BadValidationError(`This config "${config}" is invalid!`);
   }
@@ -47,9 +52,9 @@ try {
   handlerError(err);
 }
 
+const read = inputs ? new MyReadStream(nameInput) : process.stdin;
+const write = outputs ? new MyWriteStream(nameOutput) : process.stdout;
 const arrTransform = parserConfig(config);
-const read = inputs ? new MyReadStream(fileForRead) : process.stdin;
-const write = outputs ? new MyWriteStream(fileForWrite) : process.stdout;
 
 pipeline(
   read,
