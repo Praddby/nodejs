7c1ac45e76c302c8978a9dc39df797206bed0adf
Yury Bialou <Pradd.by@gmail.com>
2021-11-09 19:41:51 +0300

    feat: add error not permitted

--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,12 @@ const NotFoundError = require("./NotFoundError");
 const DuplicateError = require("./DuplicateError");
 const BadPathError = require("./BadPathError");
 const BadValidationError = require("./BadValidationError");
+const NotPermittedError = require("./NotPermittedError");
 
 module.exports = {
   NotFoundError,
   DuplicateError,
   BadPathError,
   BadValidationError,
+  NotPermittedError,
 };
