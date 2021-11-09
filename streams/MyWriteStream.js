90cbb39659915c437c57152277406a0211f90133
Yury Bialou <Pradd.by@gmail.com>
2021-11-09 19:45:21 +0300

    refactor: refactor write stream

--- streams/MyWriteStream.js
+++ streams/MyWriteStream.js
@@ -1,35 +1,40 @@
 const { Writable } = require('stream');
 const fs = require('fs');
 const path = require('path');
+const { NotPermittedError } = require('../errors');
 
 class MyWriteStream extends Writable {
   constructor(path, opt) {
     super(opt);
     this._path = path;
+    this._fd = null;
   }
 
   _construct(callback) {
     fs.open(this._path, "a+", (err, fd) => {
       if (err) {
-        this.handlerError();
+        callback(err);
       }
       this._fd = fd;
       callback();
     });
   }
 
-  handlerError() {
-    process.stderr.write(`Operation not permitted: Can't write file "${path.basename(this._path)}"!`);
-    process.exit(1);
-  }
-
   _write(chunk, _, callback) {
     fs.write(this._fd, chunk.toString(), (err) => {
       if (err) {
-        this.handlerError();
+        this.destroy(err);
       }
     })
     callback();
+  }
+
+  _destroy(err, callback) {
+    if (this._fd) {
+      fs.close(this._fd, (er) => callback(er || err));
+    } else {
+      callback(new NotPermittedError(`Can't write file "${path.basename(this._path)}"!`));
+    }
   }
 }
 
