3d4dd07c4b611ee6f220a361b2df9456bbb8926a
Yury Bialou <Pradd.by@gmail.com>
2021-11-09 17:49:05 +0300

    refactor: refactor my write stream

--- streams/MyWriteStream.js
+++ streams/MyWriteStream.js
@@ -8,22 +8,27 @@ class MyWriteStream extends Writable {
     this._path = path;
   }
 
+  _construct(callback) {
+    fs.open(this._path, "a+", (err, fd) => {
+      if (err) {
+        this.handlerError();
+      }
+      this._fd = fd;
+      callback();
+    });
+  }
+
   handlerError() {
     process.stderr.write(`Operation not permitted: Can't write file "${path.basename(this._path)}"!`);
     process.exit(1);
   }
 
   _write(chunk, _, callback) {
-    fs.open(this._path, "a+", (err, fd) => {
+    fs.write(this._fd, chunk.toString(), (err) => {
       if (err) {
         this.handlerError();
       }
-      fs.write(fd, chunk.toString(), (err) => {
-        if (err) {
-          this.handlerError();
-        }
-      })
-    });
+    })
     callback();
   }
 }
