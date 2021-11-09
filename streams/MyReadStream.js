f8ccce3b47b40b9f01dd57d0a58b6ce1bbae09f2
Yury Bialou <Pradd.by@gmail.com>
2021-11-09 19:42:07 +0300

    refactor: refactor read stream

--- streams/MyReadStream.js
+++ streams/MyReadStream.js
@@ -1,49 +1,56 @@
 const { Readable } = require('stream');
 const fs = require('fs');
 const path = require('path');
+const { NotPermittedError } = require('../errors');
 
 class MyReadStream extends Readable {
   constructor(path, opt) {
     super(opt);
     this._path = path;
+    this._fd = null;
+    this._sizeFile = 0;
+    this._start = 0;
   }
 
   _construct(callback) {
-    fs.stat(this._path, (err, stats) => {
+    fs.open(this._path, "r+", (err, fd) => {
       if (err) {
-        this.handlerError();
+        callback(err);
       }
-      this._sizeFile = stats.size;
-    })
-    this._start = 0;
-    callback();
-  }
-
-  handlerError() {
-    process.stderr.write(`Operation not permitted: Can't read file "${path.basename(this._path)}"!`);
-    process.exit(1);
+      this._fd = fd;
+      fs.stat(this._path, (err, stats) => {
+        if (err) {
+          callback(err);
+        }
+        this._sizeFile = stats.size;
+        callback();
+      });
+    });
   }
 
   async _read(size) {
-    fs.open(this._path, "r+", (err, fd) => {
-      if (err) {
-        this.handlerError();
-      }
-      const readSize = this._sizeFile < size ? this._sizeFile : size;
-      let buff = Buffer.alloc(readSize);
-      if (this._sizeFile > 0) {
-        fs.read(fd, buff, 0, readSize, this._start, (err, _) => {
-          if (err) {
-            this.handlerError();
-          }
-          this.push(buff.toString());
-        });
-        this._sizeFile -= readSize;
-        this._start = readSize;
-      } else {
-        fs.close(fd);
-      }
-    });
+    const readSize = this._sizeFile < size ? this._sizeFile : size;
+    let buff = Buffer.alloc(readSize);
+    if (this._sizeFile > 0) {
+      fs.read(this._fd, buff, 0, readSize, this._start, (err, _) => {
+        if (err) {
+          this.destroy(err);
+        }
+        this.push(buff.toString());
+      });
+      this._sizeFile -= readSize;
+      this._start = readSize;
+    } else {
+      fs.close(this._fd);
+    }
+  }
+
+  _destroy(err, callback) {
+    if (this._fd) {
+      fs.close(this._fd, (er) => callback(er || err));
+    } else {
+      callback(new NotPermittedError(`Can't read file "${path.basename(this._path)}"!`));
+    }
   }
 }
 
