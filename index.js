8af719154a756e1a7b28c643275a31b2bfb2ceb5
Yury Bialou <Pradd.by@gmail.com>
2021-11-16 14:17:19 +0300

    fix: fix validation conditions of task 1

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ try {
     throw new NotFoundError("Missing '-c' or '--config' option!")
   } else if (!config) {
     throw new NotFoundError("Missing value for '-c' or '--config' option!");
-  } else if (!nameInput) {
+  } else if (inputs && !nameInput) {
     throw new NotFoundError("Missing value for '-i' or '--input' option!");
-  } else if (!nameOutput) {
+  } else if (outputs && !nameOutput) {
     throw new NotFoundError("Missing value for '-o' or '--output' option!");
   } else if (isDuplicateOption(configs)) {
     throw new DuplicateError("Duplicated config option!");
@@ -41,9 +41,9 @@ try {
     throw new DuplicateError("Duplicated input option!");
   } else if (outputs && isDuplicateOption(outputs)) {
     throw new DuplicateError("Duplicated output option!");
-  } else if (isDir(nameInput)) {
+  } else if (nameInput && isDir(nameInput)) {
     throw new BadPathError(`${nameInput} is directory, but need file!`);
-  } else if (isDir(nameOutput)) {
+  } else if (nameOutput && isDir(nameOutput)) {
     throw new BadPathError(`${nameOutput} is directory, but need file!`);
   } else if (!validationConfig(config)) {
     throw new BadValidationError(`This config "${config}" is invalid!`);
