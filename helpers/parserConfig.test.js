cf8f8c94ab0ea43f5e656ea6ed6458b7841577a7
Yury Bialou <Pradd.by@gmail.com>
2021-11-20 12:17:37 +0300

    refactor: refactor test parser config

--- helpers/parserConfig.test.js
+++ helpers/parserConfig.test.js
@@ -8,20 +8,20 @@ const {
 describe("Helpers: testing parser config function", () => {
   test("should return instanceof AtbashTransformStream", () => {
     const obfectA = parserConfig("A")[0];
-    expect(obfectA instanceof AtbashTransformStream).toBeTruthy();
+    expect(obfectA).toBeInstanceOf(AtbashTransformStream);
   });
 
   test("should return instanceof CaesarTransformStream", () => {
     const obfectC0 = parserConfig("C0")[0];
     const obfectC1 = parserConfig("C1")[0];
-    expect(obfectC0 instanceof CaesarTransformStream).toBeTruthy();
-    expect(obfectC1 instanceof CaesarTransformStream).toBeTruthy();
+    expect(obfectC0).toBeInstanceOf(CaesarTransformStream);
+    expect(obfectC1).toBeInstanceOf(CaesarTransformStream);
   });
 
   test("should return instanceof ROTTransformStream", () => {
     const obfectR0 = parserConfig("R0")[0];
     const obfectR1 = parserConfig("R1")[0];
-    expect(obfectR0 instanceof ROTTransformStream).toBeTruthy();
-    expect(obfectR1 instanceof ROTTransformStream).toBeTruthy();
+    expect(obfectR0).toBeInstanceOf(ROTTransformStream);
+    expect(obfectR1).toBeInstanceOf(ROTTransformStream);
   });
 });
